14ae2aa4721a64123f382698eac8c608e23eb56b
mbredetss <51057964+mbredetss@users.noreply.github.com>
2023-01-31 23:57:58 -0800

    Add files via upload

---
+++ JavaApplication46.java
@@ -0,0 +1,176 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication46;
+
+/**
+ *
+ * @author Administrator
+ */
+public class JavaApplication46 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i=1;i<=8;i++ )
+        {
+          System.out.print("*");
+            if (i==1)
+            {
+                for (int j=1;j<=2;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+            else if (i==6)
+            {
+                for (int f=1;f<=2; f++)
+                System.out.print(" ");
+            }
+            else if (i==7)
+            {
+                for (int f=1;f<=5; f++)
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+        //BARIS PERTAMA
+        
+        for (int i=1;i<=7;i++)
+        {
+            System.out.print("*");
+            if (i==1)
+            {
+                for (int j=1;j<=2;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+            else if (i==2)
+            {
+                for (int j=1;j<=3;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+            else if (i==3)
+            {
+                for (int j=1;j<=2;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+            else if (i==4)
+            {
+                System.out.print(" ");
+            }
+            else if (i==5)
+            {
+               System.out.print(" ");
+            }
+            else if (i==6)
+            {
+               System.out.print(" ");
+            }
+            
+        }
+        System.out.println();
+        //BARIS KE-DUA
+        
+        
+        for (int i=1;i<=9;i++)
+        {
+            System.out.print("*");
+            if (i==1)
+            {
+                for (int j=1;j<=2;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+            else if (i==6)
+            {
+                for (int j=1;j<=2;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+            else if (i==7)
+            {
+                for (int j=1;j<=2;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+            else if (i==8)
+            {
+                for (int j=1;j<=2;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+        }
+        System.out.println();
+        //BARIS KE-TIGA
+        
+        
+        for (int i=1;i<=4;i++)
+        {
+            System.out.print("*");
+            if (i==1)
+            {
+                for (int j=1;j<=6;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+            else if (i==2)
+            {
+               for (int j=1;j<=2;j++)
+                {
+                    System.out.print(" ");
+                } 
+            }
+            else if (i==3)
+            {
+               for (int j=1;j<=5;j++)
+                {
+                    System.out.print(" ");
+                } 
+            }
+        }
+        System.out.println();
+        //BARIS KE-EMPAT
+        
+        for (int i=1;i<=8;i++)
+        {
+            System.out.print("*");
+            if (i==1)
+            {
+                for (int j=1;j<=2;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+            if (i==6)
+            {
+                for (int j=1;j<=2;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+            if (i==7)
+            {
+                for (int j=1;j<=5;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+        }
+    }
+    
+    
+}
