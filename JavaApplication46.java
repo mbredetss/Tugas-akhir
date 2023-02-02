95b0af00dc5d2f034a3f07aec93d0b98b22e48a3
mbredetss <51057964+mbredetss@users.noreply.github.com>
2023-02-02 05:12:44 -0800

    Update JavaApplication46.java

--- JavaApplication46.java
+++ JavaApplication46.java
@@ -15,160 +15,217 @@ public class JavaApplication46 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        for (int i=1;i<=8;i++ )
+    //BARIS PERTAMA
+        for (int i=1;i<=15;i++)
         {
-          System.out.print("*");
-            if (i==1)
+            System.out.print("*");
+            if (i==3)
             {
-                for (int j=1;j<=2;j++)
+                for (int j=1;j<=4;j++)
                 {
                     System.out.print(" ");
                 }
             }
-            else if (i==6)
+            else if (i==13)
             {
-                for (int f=1;f<=2; f++)
-                System.out.print(" ");
+                for (int j=1;j<=4;j++)
+                {
+                    System.out.print(" ");
+                }
             }
-            else if (i==7)
+            else if (i==14)
             {
-                for (int f=1;f<=5; f++)
-                System.out.print(" ");
+                for (int j=1;j<=11;j++)
+                {
+                    System.out.print(" ");
+                }
             }
         }
         System.out.println();
-        //BARIS PERTAMA
+    
         
-        for (int i=1;i<=7;i++)
+     //BARIS KE-DUA
+        for (int i=1;i<=8;i++)
         {
             System.out.print("*");
-            if (i==1)
+            if (i==3)
             {
-                for (int j=1;j<=2;j++)
-                {
-                    System.out.print(" ");
-                }
-            }
-            else if (i==2)
-            {
-                for (int j=1;j<=3;j++)
-                {
-                    System.out.print(" ");
-                }
-            }
-            else if (i==3)
-            {
-                for (int j=1;j<=2;j++)
+                for (int j=4;j<=15;j++)
                 {
                     System.out.print(" ");
                 }
             }
             else if (i==4)
             {
-                System.out.print(" ");
+                for (int j=1;j<=5;j++)
+                {
+                    System.out.print(" ");
+                }
             }
             else if (i==5)
             {
-               System.out.print(" ");
-            }
-            else if (i==6)
-            {
-               System.out.print(" ");
-            }
-            
-        }
-        System.out.println();
-        //BARIS KE-DUA
-        
-        
-        for (int i=1;i<=9;i++)
-        {
-            System.out.print("*");
-            if (i==1)
-            {
-                for (int j=1;j<=2;j++)
+                for (int j=1;j<=3;j++)
                 {
                     System.out.print(" ");
                 }
             }
             else if (i==6)
             {
-                for (int j=1;j<=2;j++)
+                for (int j=1;j<=3;j++)
                 {
                     System.out.print(" ");
                 }
             }
             else if (i==7)
             {
-                for (int j=1;j<=2;j++)
-                {
-                    System.out.print(" ");
-                }
-            }
-            else if (i==8)
-            {
-                for (int j=1;j<=2;j++)
+                for (int j=1;j<=3;j++)
                 {
                     System.out.print(" ");
                 }
             }
         }
-        System.out.println();
-        //BARIS KE-TIGA
-        
-        
-        for (int i=1;i<=4;i++)
+    
+     System.out.println();
+     //BARIS KET-TIGA
+        for (int i=1;i<=8;i++)
         {
             System.out.print("*");
-            if (i==1)
+            if (i==3)
+            {
+                for (int j=4;j<=14;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+            else if (i==4)
             {
                 for (int j=1;j<=6;j++)
                 {
                     System.out.print(" ");
                 }
             }
-            else if (i==2)
+            else if (i==5)
             {
-               for (int j=1;j<=2;j++)
+                for (int j=1;j<=3;j++)
                 {
                     System.out.print(" ");
-                } 
+                }
             }
-            else if (i==3)
+            else if (i==6)
             {
+                for (int j=1;j<=3;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+            else if (i==7)
+            {
+                for (int j=1;j<=3;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+        }
+        
+        
+        System.out.println();
+    //BARIS KE-EMPAT
+        for (int i=1;i<=9;i++)
+        {
+            System.out.print("*");
+            if (i==3)
+            {
+                for (int j=4;j<=13;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+            else if (i==4)
+            {
+                for (int j=1;j<=7;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+            else if (i==5)
+            {
+                for (int j=1;j<=4;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+            else if (i==8)
+            {
+                for (int j=1;j<=4;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+        }
+        
+        
+        System.out.println();
+    //BARIS KE-4
+        for (int i=1;i<=7;i++)
+        {
+           System.out.print("*");
+           if (i==3)
+            {
+                for (int j=4;j<=12;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+           else if (i==4)
+            {
+                for (int j=1;j<=8;j++)
+                {
+                    System.out.print(" ");
+                }
+            }
+           else if (i==5)
+           {
                for (int j=1;j<=5;j++)
                 {
                     System.out.print(" ");
-                } 
-            }
+                }
+           }
+           else if (i==6)
+           {
+               for (int j=1;j<=5;j++)
+                {
+                    System.out.print(" ");
+                }
+           }
         }
-        System.out.println();
-        //BARIS KE-EMPAT
         
-        for (int i=1;i<=8;i++)
+        
+        System.out.println();
+    //BARIS KE-LIMA
+        for (int i=1;i<=6;i++)
         {
             System.out.print("*");
-            if (i==1)
+            if (i==3)
             {
-                for (int j=1;j<=2;j++)
+                for (int j=4;j<=11;j++)
                 {
                     System.out.print(" ");
                 }
             }
-            if (i==6)
+           else if (i==4)
             {
-                for (int j=1;j<=2;j++)
+                for (int j=1;j<=9;j++)
                 {
                     System.out.print(" ");
                 }
             }
-            if (i==7)
-            {
-                for (int j=1;j<=5;j++)
+           else if (i==5)
+           {
+               for (int j=1;j<=11;j++)
                 {
                     System.out.print(" ");
                 }
-            }
+           }
         }
     }
     
